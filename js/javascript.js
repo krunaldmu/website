3c11c1c189b327203a9e93bfbf2f46412a4cb021
Krunal Vomexchandra <p13205072@my365.dmu.ac.uk>
2017-02-28 23:38:41 +0000

    idented code for pages

--- js/javascript.js
+++ js/javascript.js
@@ -6,99 +6,106 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 // script for smooth mouse scrolling //
-            $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                scrollTop: target.offset().top
+                    scrollTop: target.offset().top
                 }, 1000);
                 return false;
             }
-            }
-        });
-        });
-// script for image slideshow //
-          var myIndex = 0;
-          carousel();
-
-          function carousel() {
-              var i;
-              var x = document.getElementsByClassName("slides");
-              for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";  
-              }
-              myIndex++;
-              if (myIndex > x.length) {myIndex = 1}    
-              x[myIndex-1].style.display = "block";  
-              setTimeout(carousel, 9000);
-          }
-// script for google map api
-    function myMap() {
-          var myCenter = new google.maps.LatLng(20.72,70.94);
-          var mapCanvas = document.getElementById("map");
-          var mapOptions = {center: myCenter, zoom: 13};
-          var map = new google.maps.Map(mapCanvas, mapOptions);
-          var marker = new google.maps.Marker({position:myCenter});
-          marker.setMap(map);
         }
+    });
+});
+// script for image slideshow //
+var myIndex = 0;
+carousel();
 
-                $(document).ready(function() {
-            $("#lightSlider").lightSlider({
-                item: 1,
-                autoWidth: false,
-                slideMove: 1, // slidemove will be 1 if loop is true
-                slideMargin: 10,
-        
-                addClass: '',
-                mode: "slide",
-                useCSS: true,
-                cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-                easing: 'linear', //'for jquery animation',////
-        
-                speed: 400, //ms'
-                auto: true,
-                loop: true,
-                slideEndAnimation: true,
-        
-                keyPress: false,
-                controls: true,
-        
-                vertical:false,
-                verticalHeight:500,
-                vThumbWidth:100,
-        
-                thumbItem:10,
-                pager: true,
-                gallery: false,
-                galleryMargin: 5,
-                thumbMargin: 5,
-                currentPagerPosition: 'middle',
-        
-                enableTouch:true,
-                enableDrag:true,
-                freeMove:true,
-                swipeThreshold: 40,
-        
-                responsive : [],
-        
-                onBeforeStart: function (el) {},
-                onSliderLoad: function (el) {},
-                onBeforeSlide: function (el) {},
-                onAfterSlide: function (el) {},
-                onBeforeNextSlide: function (el) {},
-                onBeforePrevSlide: function (el) {}
-            });
-        });
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 9000);
+}
+// script for google map api
+function myMap() {
+    var myCenter = new google.maps.LatLng(20.72, 70.94);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: myCenter,
+        zoom: 13
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCenter
+    });
+    marker.setMap(map);
+}
+
+$(document).ready(function() {
+    $("#lightSlider").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 10,
+
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+
+        speed: 400, //ms'
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+
+        keyPress: false,
+        controls: true,
+
+        vertical: false,
+        verticalHeight: 500,
+        vThumbWidth: 100,
+
+        thumbItem: 10,
+        pager: true,
+        gallery: false,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+
+        enableTouch: true,
+        enableDrag: true,
+        freeMove: true,
+        swipeThreshold: 40,
+
+        responsive: [],
+
+        onBeforeStart: function(el) {},
+        onSliderLoad: function(el) {},
+        onBeforeSlide: function(el) {},
+        onAfterSlide: function(el) {},
+        onBeforeNextSlide: function(el) {},
+        onBeforePrevSlide: function(el) {}
+    });
+});
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-        function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
